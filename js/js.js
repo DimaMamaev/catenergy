ead0d178e2801db1e30f10d8d84257e9830d783f
DimaMamaev <dmamaev@my.com>
2019-07-21 15:00:33 +0300

    initial commit

---
+++ js/js.js
@@ -0,0 +1,8 @@
+var link = document.querySelector('.menu-btn--active');
+var menu = document.querySelector('.page-header__usernav--active');
+    
+    link.addEventListener('click',function(event){
+    event.preventDefault();
+    menu.classList.toggle('page-header__usernav--active');
+    link.classList.toggle('menu-btn--active');
+    })
