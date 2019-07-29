a5f87e7d1311b84391303e941eea6296b0a83a43
DimaMamaev <dmamaev@my.com>
2019-07-29 21:36:30 +0300

    initial commit

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+
+  $('.owl-carousel').owlCarousel({
+    items:1,
+    nav: true,
+    stagePadding: 50,
+    navText: [ 'БЫЛО', 'СТАЛО']
+})
+ 
