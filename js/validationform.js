6dcdd329981ae329970c24f626676edc2e73127a
DimaMamaev <dmamaev@my.com>
2019-11-03 12:01:59 +0200

    validation of data in input form

---
+++ js/validationform.js
@@ -0,0 +1,12 @@
+var catNameInput = document.querySelector('.section__input');
+
+catNameInput.addEventListener('invalid', function
+(evt){
+    if (catNameInput.validity.tooShort) {
+        catNameInput.setCustomValidity ('Имя/кличка кота должна состоять минимум из трех символов');} 
+    else if (catNameInput.validity.tooLong) {
+        catNameInput.setCustomValidity ('Имя/кличка кота должна состоять максимум из пятнадцати символов');}
+    else if (catNameInput.validity.valueMissing) {
+        catNameInput.setCustomValidity ('Это поле обязательное для заполнения');}
+}
+)
